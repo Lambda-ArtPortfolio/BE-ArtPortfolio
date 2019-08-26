65427837fc40bd20acf2c57343fc1a6b0f657d92
Nate Magera <nmagera07@gmail.com>
2019-08-26 11:38:37 -0400

    few changes

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ server.use(cors())
 server.use(express.json())
 
 server.use('/auth', authRouter)
+server.use('/', authRouter)
 
 server.get('/', (req, res) => {
     res.status(200).json({ message: 'Hello you are up and running!' })
