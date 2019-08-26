5fad098b353617a327a5707c5d599f0c7982ee07
Nate Magera <nmagera07@gmail.com>
2019-08-26 11:06:51 -0400

    init

---
+++ auth/auth-router.js
@@ -0,0 +1,59 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const secrets = require('./secrets')
+
+const Users = require('./auth-model')
+
+router.post('/register', (req, res) => {
+  let user = req.body
+
+  const hash = bcrypt.hashSync(user.password)
+  user.password = hash
+
+  Users.add(user)
+    .then(addedUser => {
+      const token = generateToken(user)
+      const added = { username: user.username}
+      res.status(201).json({ added, token})
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Failed to register user', error})
+    })
+});
+
+router.post('/login', (req, res) => {
+  let { username, password } = req.body
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user)
+        res.status(200).json({ message: `Welcome to the app, ${user.username}`, token})
+      } else {
+        res.status(401).json({ message: 'Please enter correct user credentials to login.'})
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Failed to login!', error})
+    })
+});
+
+
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+
+  }
+
+  const options = {
+    expiresIn: '1d',
+  }
+
+  return jwt.sign(payload, secrets.jwtSecret, options)
+}
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+
+const authenticate = require('./auth/auth-middleware.js')
+const authRouter = require('./auth/auth-router.js')
+
+const server = express()
+
+server.use(helmet())
+server.use(cors())
+server.use(express.json())
+
+server.use('/auth', authRouter)
+
+server.get('/', (req, res) => {
+    res.status(200).json({ message: 'Hello you are up and running!' })
+})
+
+module.exports = server
